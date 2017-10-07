d6f529c7daf8345771a9ee5c23f9ec6ac7f674f9
DunnCreativeSS <jarett+github@dcssquared.com>
2017-10-07 05:01:18 -0300

    220

--- etherselfarb.js
+++ etherselfarb.js
@@ -182,7 +182,7 @@ function lala(tokenAddr, tokencount){
 						try {
 							if (threshold > .12){
 							buyit(tokenAddr, tokencount, threshold, edSells, winSp);
-							sleep(180000);
+							sleep(220000);
 							}
 					} catch (err) {
 						console.log(err);
