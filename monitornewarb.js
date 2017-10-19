ea4919c2eca93b179fbf5a6683aa7b3dbc35bd7a
DunnCreativeSS <jarett+github@dcssquared.com>
2017-10-19 02:42:30 -0300

    var //sleep = require('system-//sleep');

--- monitornewarb.js
+++ monitornewarb.js
@@ -43,7 +43,6 @@ var cheerio = require('cheerio');
 var math = require("mathjs");
 var BigNumber = require("bignumber.js");
 var request = require("request");
-var //sleep = require('system-//sleep');
 var crypto = require("crypto");
 var Web3 = require("web3");
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
